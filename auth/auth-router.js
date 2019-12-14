b4a0994a310ef634d54e0f0ddcf46179f08b4611
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-12-13 21:23:08 -0500

    added dependicies and made helper functions

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const Users = require("../");
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post("/register", (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
 
